003698280bb39c6581e15ebbad053f2fa11d064e
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-04-04 02:30:17 -0300

    #1-release: configuring axios & theme

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,35 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>
+    <Header/>
+    {{ruleList || "Nenhum dado encontrado."}}
+  </div>
 </template>
 
 <script>
+
 export default {
-  name: 'IndexPage'
-}
+  name: 'IndexPage',
+  data() {
+    return {
+      ruleList: []
+    }
+  }, 
+  mounted() {
+    this.getRulesList();
+  },
+  methods: {
+    async getRulesList() {
+      try {
+        const token = window.localStorage.getItem("token")
+        if (token) {
+          this.$axios.setToken(token, 'Bearer');
+          const {data} = await this.$axios.$get(`/admin/house_rules`)
+          this.ruleList = data;
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  }
+ }
 </script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,21 @@ module.exports = {
     "./plugins/**/*.{js,ts}",
     "./nuxt.config.{js,ts}",
   ],
+  darkMode: "class",
+  variants: {
+    backgroundColor: [
+      "dark",
+      "dark-hover",
+      "dark-group-hover",
+      "dark-even",
+      "dark-odd"
+    ],
+    borderColor: ["dark", "dark-focus", "dark-focus-within"],
+    textColor: ["dark", "dark-hover", "dark-active"]
+  },
   theme: {
-    extend: {},
   },
   plugins: [],
+
 }
 
